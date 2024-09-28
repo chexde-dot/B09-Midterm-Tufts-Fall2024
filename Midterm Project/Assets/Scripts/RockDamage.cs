e9bc3d0dc40803ddf016aa925db0da71acb078d1
Fcagua <fcagua01@tufts.edu>
2024-09-27 22:30:38 -0400

    Rocks and UI started
    added GameHandler, UI, and first rock hazard.

---
+++ Midterm Project/Assets/Scripts/RockDamage.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockDamage : MonoBehaviour
+{
+    public GameHandler gameHandlerObj;
+    public int damage = 1;
+    //public Transform backToStart; //uncomment this line for "auto-death," to zap the Player back to start
+
+    void Start()
+    {
+        if (GameObject.FindWithTag("GameHandler") != null)
+        {
+            gameHandlerObj = GameObject.FindWithTag("GameHandler").GetComponent<GameHandler>();
+        }
+    }
+
+    public void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            gameHandlerObj.playerGetHit(damage);
+            //other.transform.position = new Vector3(backToStart.position.x, backToStart.position.y, backToStart.position.z);
+        }
+    }
+}
