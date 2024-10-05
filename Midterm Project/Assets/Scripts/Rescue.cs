993f666ba22b6db3a9a9826f1e8498af3ae0e3d8
XenonJ <90894424+XenonJ@users.noreply.github.com>
2024-10-04 21:27:08 -0400

    add a Controller script and import a water package

--- Midterm Project/Assets/Scripts/Rescue.cs
+++ Midterm Project/Assets/Scripts/Rescue.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Rescue : MonoBehaviour
 {
-    void OnCollisionEnter(Collision collision)
+    void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
